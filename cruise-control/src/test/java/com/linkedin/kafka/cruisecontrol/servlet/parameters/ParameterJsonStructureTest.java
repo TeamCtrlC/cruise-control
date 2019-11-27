9664410c1f732952d41ffce78f7a27b028dbefbe
ian952 <ianhu952@hotmail.com>
2019-11-26 22:52:48 -0800

    working test

--- cruise-control/src/test/java/com/linkedin/kafka/cruisecontrol/servlet/parameters/ParameterJsonStructureTest.java
+++ cruise-control/src/test/java/com/linkedin/kafka/cruisecontrol/servlet/parameters/ParameterJsonStructureTest.java
@@ -12,29 +12,25 @@ import io.swagger.v3.parser.OpenAPIV3Parser;
 
 import io.swagger.v3.parser.core.models.ParseOptions;
 import io.swagger.v3.parser.core.models.SwaggerParseResult;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class ParameterJsonStructureTest {
-
-
   private Map<String, Class> _endpointToClass;
   OpenAPI _openAPI;
   @Before
-  public void setupParameterClasses() throws Exception {
+  public void setupParameterClasses() {
     _endpointToClass = new HashMap<>();
+    _endpointToClass.put("/kafkacruisecontrol/partition_load", PartitionLoadParameters.class);
+    _endpointToClass.put("/kafkacruisecontrol/rebalance", RebalanceParameters.class);
     _endpointToClass.put("/kafkacruisecontrol/state", CruiseControlStateParameters.class);
-    System.out.println(_endpointToClass.get("/kafkacruisecontrol/state").getMethod("getCaseInsensitiveParameterNames").invoke(null));
     }
 
   @Test
-  public void loadOpenApiSpec() {
+  public void loadOpenApiSpec() throws Exception {
     OpenAPIV3Parser openApiParser = new OpenAPIV3Parser();
     String baseFileName = "../schemas/base.yaml";
     ParseOptions options = new ParseOptions();
@@ -44,6 +40,10 @@ public class ParameterJsonStructureTest {
     SwaggerParseResult parseResult = openApiParser.readLocation(baseFileName, null, options);
     _openAPI = parseResult.getOpenAPI();
     Schema schema = parseSchema();
+    for (String endpoint: schema.getEndpoints()) {
+      Assert.assertTrue(_endpointToClass.containsKey(endpoint));
+      Assert.assertEquals(schema.getParameters(endpoint), ((CruiseControlParameters) (_endpointToClass.get(endpoint).newInstance())).caseInsensitiveParameterNames());
+    }
 //    System.out.println(schema);
   }
 
@@ -55,7 +55,7 @@ public class ParameterJsonStructureTest {
    */
   public static Set<String> parseEndpoint(PathItem pathItem) throws IllegalArgumentException {
     List<Parameter> parameterList;
-    Set<String> parameterSet = new HashSet<>();
+    Set<String> parameterSet = new TreeSet<>();
     if (pathItem.getGet() != null) {
       parameterList = pathItem.getGet().getParameters();
     } else if (pathItem.getPost() != null) {
@@ -136,47 +136,4 @@ public class ParameterJsonStructureTest {
       return stringBuilder.toString();
     }
   }
-
-//  void checkSchema(Schema schema, String name, boolean isRoot) {
-//    // get the annotated fields in Java class
-//    System.out.println(name);
-//    Map<String, Boolean> fields = extractAnnotatedFields(_schemaToClass.get(name));
-//
-//    // check property are consistent
-//    Map<String, Schema> properties = schema.getProperties();
-//
-//    // Check version field
-//    if (isRoot) {
-//      assertEquals(properties.size(), fields.size() + 1);
-//      assertTrue(properties.get(VERSION) instanceof IntegerSchema);
-//    } else {
-//      if (properties.size() - fields.size() == 1) {
-//        assertTrue(properties.get(VERSION) instanceof IntegerSchema);
-//      } else {
-//        assertEquals(String.format("%s \n %s", properties.keySet(), fields), properties.size(), fields.size());
-//      }
-//    }
-//
-//    // Check other fields
-//    properties.forEach((k, v) -> {
-//      if (!k.equals(VERSION)) {
-//        System.out.println(k + " -> " + v.getClass().getSimpleName());
-//        assertTrue(k + " does not exist", fields.containsKey(k));
-//        assertTrue(k, (fields.get(k) == (schema.getRequired() != null && schema.getRequired().contains(k))));
-//        // Check value
-//        Schema schemaToCheck = v instanceof ArraySchema ? ((ArraySchema) v).getItems() :
-//                v instanceof MapSchema ? (Schema) v.getAdditionalProperties() : v;
-//        if (schemaToCheck instanceof StringSchema ||
-//                schemaToCheck instanceof IntegerSchema ||
-//                schemaToCheck instanceof NumberSchema ||
-//                schemaToCheck instanceof ComposedSchema) {
-//          return;
-//        }
-//        assertNotEquals(schemaToCheck.get$ref(), null);
-//        String refName = schemaToCheck.get$ref();
-//        String className = refName.substring(refName.lastIndexOf('/') + 1);
-//        checkSchema(_openAPI.getComponents().getSchemas().get(className), className, false);
-//      }
-//    });
-//  }
 }
